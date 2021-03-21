d155fcbaee3a6620135bba06a18ae196e23449dc
elliefh <elliefu18@gmail.com>
2021-03-20 18:07:38 -0700

    added Routes for each page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,25 @@ import MyNavbar from "./components/Navbar";
 import MyHeader from "./components/Header";
 import Container from 'react-bootstrap/Container';
 import Footer from "./components/Footer";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <MyHeader/>
       <MyNavbar/>
+      <Container>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Container>
       <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
