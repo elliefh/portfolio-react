273163dfe4826345911d11682f6c294a5ab6cee9
elliefh <elliefu18@gmail.com>
2021-03-20 18:24:47 -0700

    added link to resume

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -2,7 +2,6 @@ import React from "react";
 // import ReactDOM from 'react-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
@@ -14,7 +13,7 @@ function MyNavbar() {
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="https://docs.google.com/document/d/1EFb6fD7jt9nr4023E_L3wkIkPHb7o3mSGsQcHiFlllY/edit?usp=sharing">Resume</Nav.Link>
         </Nav>
     </Navbar>
     )
