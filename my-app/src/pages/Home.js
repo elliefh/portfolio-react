b8508ed4e19b31bc4cbbd54dec6875d1b5991678
elliefh <elliefu18@gmail.com>
2021-03-22 09:26:11 -0700

    added carousel component

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import MyCarousel from "../components/Carousel";
 
 function Home() {
     return(
-        <div>Welcome to my home page</div>
+        <Container>
+            <MyCarousel></MyCarousel>
+        </Container>
     )
 }
 
