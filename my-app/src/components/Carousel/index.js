e428cdd4b155da7678257de62956a45d68fb7e92
elliefh <elliefu18@gmail.com>
2021-03-22 09:25:29 -0700

    updated carousel compoent + styling

--- my-app/src/components/Carousel/index.js
+++ my-app/src/components/Carousel/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import './style.css';
 
 function MyCarousel(props) {
     return(
+    <Carousel>
+
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src={props.image}
-            alt="Project Screenshot"
+            src="https://images.unsplash.com/photo-1595853035070-59a39fe84de3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8d2VsY29tZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="First slide"
+            width="720"
+            height="540"
             />
             <Carousel.Caption>
-            <h3>{props.name}</h3>
-            <p>{props.description}</p>
+            <h3>Welcome</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
-            <Button variant="link" href="#">{props.repoLink}</Button>
-            <Button variant="link" href="#">{props.liveLink}</Button>
         </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VhdGhlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="Second slide"
+            width="720"
+            height="540"
+            />
+            <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+    </Carousel>
     )
 }
 
