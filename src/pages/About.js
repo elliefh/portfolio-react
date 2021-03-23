4f5b1a9147c4fc987dd6ffcb3c815c193af10282
elliefh <elliefu18@gmail.com>
2021-03-23 14:34:04 -0700

    re-doing gh pages deployment

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,7 +15,7 @@ function About() {
             </Row>
             <Row className = "d-flex justify-content-center">
                 <Col className = "d-flex justify-content-center" xs={6}>
-                    <p className="text-center">Hello and thank you for visiting my website!<br/><br/>
+                    <p className="text-center">Hello and thank you for visiting my website.<br/><br/>
                     My name is Ellie and I am currently pursuing a full stack web development certificate through the University of Washington's Coding Bootcamp Program. I am also a student at the Academy of Art University pursuing my MFA. 
                     <br/><br/>
                     You can check out some of my highlighted projects in my <a href="/portfolio">portfolio page</a>. If you have any questions or comments, please feel free to reach out to me through my <a href="/contact">contact page</a>.<br/><br/>
